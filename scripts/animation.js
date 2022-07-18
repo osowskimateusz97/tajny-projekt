5deb73a45a2513c31eb415ec0d8707f2e637b98d
Matthew <osowski.mateuszz@gmail.com>
2022-07-18 16:45:49 +0200

    Add animations

---
+++ scripts/animation.js
@@ -0,0 +1,103 @@
+function init() {
+  const loader = document.querySelector(".loader");
+
+  // reset position of the loading screen
+  gsap.set(loader, {
+    scaleX: 0,
+    rotation: 10,
+    xPercent: -5,
+    yPercent: -50,
+    transformOrigin: "left center",
+    autoAlpha: 1,
+  });
+
+  function loaderIn() {
+    // GSAP tween to stretch the loading screen across the whole screen
+    return gsap.fromTo(
+      loader,
+      {
+        rotation: 10,
+        scaleX: 0,
+        xPercent: -5,
+      },
+      {
+        duration: 0.7,
+        xPercent: 0,
+        scaleX: 1,
+        rotation: 0,
+        ease: "Power4.inOut",
+        transformOrigin: "left center",
+      }
+    );
+  }
+
+  function loaderAway() {
+    // GSAP tween to hide the loading screen
+    return gsap.to(loader, {
+      duration: 0.7,
+      scaleX: 0,
+      xPercent: 5,
+      rotation: -10,
+      transformOrigin: "right center",
+      ease: "Power4.inOut",
+    });
+  }
+
+  // do something before the transition starts
+  barba.hooks.before(() => {
+    document.querySelector("html").classList.add("is-transitioning");
+    barba.wrapper.classList.add("is-animating");
+  });
+
+  // do something after the transition finishes
+  barba.hooks.after(() => {
+    document.querySelector("html").classList.remove("is-transitioning");
+    barba.wrapper.classList.remove("is-animating");
+  });
+
+  // scroll to the top of the page
+  barba.hooks.enter(() => {
+    window.scrollTo(0, 0);
+  });
+
+  //toggle hamburger
+  function initHamburger(container) {
+    const hamburger = container.querySelector('svg[data-type="hamburger"]');
+    const navContainer = container.querySelector(
+      'nav[data-type="navigation-container"]'
+    );
+    const toggleMenuVisibility = () => {
+      navContainer.classList.toggle("mobile-hidden");
+      document.body.classList.toggle("body-overflow");
+    };
+
+    hamburger.addEventListener("click", toggleMenuVisibility);
+  }
+
+  barba.init({
+    transitions: [
+      {
+        async leave({ current }) {
+          const container = current.container;
+          const navContainer = container.querySelector(
+            ".navigation__container"
+          );
+          const isMobile = window.innerWidth <= 1025;
+
+          if (isMobile)
+            await gsap.to(navContainer, { x: "-100%", duration: 0.2 });
+          await loaderIn();
+        },
+        enter({ next }) {
+          const container = next.container;
+          loaderAway();
+          initHamburger(container);
+        },
+      },
+    ],
+  });
+}
+
+window.addEventListener("load", function () {
+  init();
+});
