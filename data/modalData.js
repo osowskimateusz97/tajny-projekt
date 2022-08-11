13cdebe52e17664fb5fdd23b625658e470218b90
Szymon-the-Cat <94720468+Szymon-the-Cat@users.noreply.github.com>
2022-08-11 15:56:33 +0200

    v.1.4.5

--- data/modalData.js
+++ data/modalData.js
@@ -1,15 +1,218 @@
 const modalData = {
-  pregnancy: {
-    title: "Prowadzenie ciąży",
-    description: `<p class='paragraph'>Prowadzenie ciazy - Opiekę nad ciężarną staramy rozpocząć się jak najwcześniej. W naszym centrum, przyszłe mamy czeka wybór ginekologa, który poprowadzi je przez cały okres ciąży.</p> <p class='paragraph'>Częstość wizyt jest indywidualnie dostosowana do potrzeb pacjentki oraz do stopnia ewentualnego ryzyka medycznego. Wizyty odbywają się w intymnej i przyjaznej atmosferze. Prowadzenie ciąży jest zindywidualizowane i dostosowane do potrzeb i oczekiwań kobiety.</p>`,
+  echoserca: {
+    title: "Echo serca płodu",
+    description: `<p class='paragraph'>Echo serca płodu to badanie przypominające zwykłe USG w ciąży. Procedura jest zupełnie
+    nieinwazyjna i bezpieczna: zarówno dla matki, jak i dziecka. Bicie serca dziecka słychać już
+    w 6 tygodniu ciąży. Organ ten jest zupełnie ukształtowany już około 8 tygodnia ciąży.
+    Naszym pacjentkom zalecamy echo serca płodu, dlatego, że anomalia tego organu dotyczą
+    około 2- 2,5 % płodów i są - obok wad układu nerwowego - najczęstszymi zaburzeniami
+    stwierdzanymi w ciąży. Diagnoza pozwala lekarzom na przygotowanie się do tego jak
+    postępować z dzieckiem.
+    Echo serca płodu warto wykonać między 18., a 22. tygodniem ciąży, gdy organy wewnętrzne
+    są już na tyle duże, że można wyraźnie zobaczyć je w badaniu.
+    
+    Specjalista, w trakcie diagnostyki może ocenić, czy rozwijające się dziecko jest wydolne
+    krążeniowo, czy funkcje czynnościowe serca i całego układu są prawidłowe. Echo serca
+    pozwala na ocenę m.in.: struktury, wielkości i symetrii tego organu. Zgodnie z aktualnymi
+    zaleceniami Polskiego Towarzystwa Ginekologicznego (Sekcji Ultrasonografii), w czasie
+    oceny serca płodu, lekarz powinien ocenić: wielkość, rytm i obraz dwóch przedsionków oraz
+    komór serca.
+    W Polsce, echo serca wykonuje niewielu lekarzy. Wolmedica współpracuje z uznanymi
+    specjalistami w tej dziedzinie, co jest gwarantem wiarygodności realizowanych w naszym
+    ośrodku badań.
+    Echo serca umożliwia również m.in.: ocenę całego płodu, lokalizację i budowę łożyska,
+    wykrycie anomalii wskazujących na możliwe choroby genetyczne.
+    </p>
+    `,
   },
   usg: {
     title: "USG 3d 2d",
-    description: `<p class='paragraph'>USG 2D I 3D - Opiekę nad ciężarną staramy rozpocząć się jak najwcześniej. W naszym centrum, przyszłe mamy czeka wybór ginekologa, który poprowadzi je przez cały okres ciąży.</p> <p class='paragraph'>Częstość wizyt jest indywidualnie dostosowana do potrzeb pacjentki oraz do stopnia ewentualnego ryzyka medycznego. Wizyty odbywają się w intymnej i przyjaznej atmosferze. Prowadzenie ciąży jest zindywidualizowane i dostosowane do potrzeb i oczekiwań kobiety.</p>`,
+    description: `<p class='paragraph'>W Wolmedica oferujemy naszym ciężarnym pacjentkom pełne spektrum badań USG.
+    USG 2D pozwala na obserwację wnętrza macicy i ogólnego zarysu rozwijającego się płodu,
+    w tym jego narządów wewnętrznych: serca, nerek, mózgu, żołądka, pęcherza moczowego.
+    USG 2D umożliwia również ocenę łożyska oraz ilości wód płodowych. Obrazy i przekroje
+    ciała płodu są płaskie i to odróżnia te technikę od badań USG 3D i 4D.
+    USG 3 D pokazuje rozwijający się płód w trójwymiarze. USG 4D to technika, która pozwala
+    na przestrzenne obrazowanie płodu, w czasie rzeczywistym.
+    Zarówno USG 3D, jak i 4D może być dużym doznaniem emocjonalnym dla rodziców. W
+    trakcie USG 3D widać precyzyjny obraz dziecka. Z kolej USG 4D to ruchomy obraz w
+    trójwymiarze. Badania te pozwalają na precyzyjniejszą niż przy USG 2D, ocenę
+    rozwijającego się: mózgowia, serca, nerek twarzy i innych organów dziecka. Widać wyraźnie
+    rączki, nóżki i całą powierzchnię płodu.
+    Obrazowanie 3D/4D nie zawsze jest jednak czytelne. Przeszkodą, by dobrze zobaczyć
+    dziecko, może być jego niekorzystne ułożenie lub ustawienie, mała objętość płynu
+    owodniowego bądź słaba przezierność tkanek ciężarnej.
+    Aparat USG którym dysponujemy umożliwia również badanie dopplerowskie, czyli ocenę
+    przepływu krwi przez naczynia matki i rozwijającego się w jej brzuchu dziecka.
+    </p>`,
   },
-  dentist: {
-    title: "Super dentysta",
-    description: `<p class='paragraph'>Super hiper fajny dentysta - Opiekę nad ciężarną staramy rozpocząć się jak najwcześniej. W naszym centrum, przyszłe mamy czeka wybór ginekologa, który poprowadzi je przez cały okres ciąży.</p> <p class='paragraph'>Częstość wizyt jest indywidualnie dostosowana do potrzeb pacjentki oraz do stopnia ewentualnego ryzyka medycznego. Wizyty odbywają się w intymnej i przyjaznej atmosferze. Prowadzenie ciąży jest zindywidualizowane i dostosowane do potrzeb i oczekiwań kobiety.</p>`,
+  badaniaprenatalne: {
+    title: "Badania prenatalne",
+    description: `<p class='paragraph'>W naszych przychodniach zapewniamy pacjentkom dostęp do pełnego zestawu badań
+    prenatalnych.
+    
+    Te są zalecane określonej grupie kobiet w ciąży:
+    
+    ● pacjentkom, które ukończyły 35 lat;
+    ● kobietom, które zaszły w ciążę z dużo starszym od siebie partnerem, zwłaszcza po
+    55. roku życia;
+    ● gdy w rodzinie matki lub ojca dziecka zdiagnozowano choroby uwarunkowane
+    genetycznie;
+    ● w przypadku zapłodnienia pozaustrojowego in vitro;
+    ● gdy ciężarna urodziła już chore dziecko.
+    Badania prenatalne pozwalają wykryć lub wykluczyć wady rozwojowe u płodu, przed
+    narodzinami.
+    
+    Pierwsze badania prenatalne można wykonać już od 10. tygodnia ciąży. Jest to nieinwazyjne i
+    bezpieczne badanie przesiewowe z krwi (NIFTY).
+    
+    Pomiędzy 14 a 21 tygodniem ciąży, można wykonać test podwójny, który polega na pobraniu
+    krwi ciężarnej i analizowaniu go pod kątem stężeniu hormonu ciążowego beta-HCG i pappa
+    
+    Wśród inwazyjnych badań prenatalnych można wyróżnić:
+    
+    - amniopunkcję (wykonuje się ją między 15 a 20 tygodniem ciąży);
+    
+    - biopsja kosmówki (CVS) – zabieg wykonywany przez powłoki brzuszne lub szyjkę macicy
+    pomiędzy 11 a 14 tygodniem ciąży;
+    
+    - kardocenteza – realizuje się ją po 20 tygodniu ciąży, polega na nakłuciu sznura
+    pępowinowego i  pobraniu krwi pępowinowej.
+    
+    W Wolmedica gwarantujemy pacjentkom również dostęp do nieinwazyjnych, bardzo
+    precyzyjnych testów prenatalnych: Nifty i Harmony.
+    
+    Nieinwazyjnymi badaniami prenatalnymi, które oferujemy naszym pacjentkom są też
+    echokardiografia płodu, która umożliwia ocenę stanu serca płodu i jego układu krążenia oraz
+    ultrasonografia genetyczna.
+    </p>`,
+  },
+
+  nifty: {
+    title: "NIFTY",
+    description: `<p class='paragraph'>To nieinwazyjne testy prenatalne, które są dostępne dla ciężarnych pacjentek, w Centrum
+    Medycznym Wolmedica. Można wykonać je już od 10. tygodnia ciąży. Wystarczy mała
+    próbka matczynej krwi żylnej.
+    
+    Test NIFTY by GenePlanet zalecamy: każdej kobiecie, która chce spokojnie przeżyć ciążę,
+    ciężarnym mających 35 lat lub więcej, pacjentkom u których inne badanie przesiewowe,
+    wykonane w pierwszym trymestrze ciąży, wykazało pośrednie ryzyko aneuploidii lub ryzyko
+    pomiędzy 1:100 a 1:1000.
+    
+    Test Nifty wykrywa szeroka gamę zaburzeń genetycznych płodu, w tym:
+    zespół Downa (trisomia 21), Edwardsa (trisomia 18) i Patau (trisomia 13),aneuploidie
+    chromosomu płci, trisomie 9, 16, 22. Dzięki testowi można poznać również płeć dziecka.
+    Test NIFTY by GenePlanet cechuje najwyższy wskaźnik wykrywalności najczęstszych
+    trisomii (powyżej 99% - w przypadku zespołu Downa).
+    
+    </p>
+    `,
+  },
+
+  harmony: {
+    title: "HARMONY",
+    description: `<p class='paragraph'>W naszym ośrodku oferujemy ciężarnym pacjentkom również dostęp do testów Harmony.
+    Podobnie, jak testy NIFTY, jest to bezpieczne badanie, wykonywane z krwi matki, które
+    pozwala wykryć określone zaburzenia chromosomalne u płodu już od 10 tygodnia ciąży. 
+    Test prenatalny Harmony określa prawdopodobieństwo występowania najczęstszych zaburzeń
+    chromosomalnych, takich jak: zespół Downa (trisomia 21), zespół Edwardsa (trisomia 18) i
+    zespół Patau (trisomia 13).
+    
+    Poza badaniem przesiewowym, w kierunku wyżej wymienionych zaburzeń, test Harmony
+    pozwala również uzyskać informacje na temat:
+    ● płci dziecka
+    ● monosomii X – nazywanej także zespołem Turnera, w którym u osoby płci żeńskiej
+    brakuje jednego chromosomu X, co może prowadzić do wad serca, zaburzeń
+    hormonalnych oraz problemów z nauką,
+    
+    ● mikrodelecji 22q11.2 – nazywanej także zespołem DiGeorge’a, zaburzenia
+    spowodowanego brakiem małych fragmentów (mikrodelecji) chromosomu 22, co
+    może prowadzić do zaburzeń pracy serca, nerek, wzrostu czy kłopotów z nauką,
+    ● aneuploidii chromosomów płciowych – zmiany liczby chromosomów X i Y, mogące
+    prowadzić do trudności w nauce i zaburzeń behawioralnych oraz innych problemów
+    zdrowotnych.
+    
+    </p>
+    `,
+  },
+
+  modelplodu: {
+    title: "Model płodu 3D",
+    description: `<p class='paragraph'>Przychodnia Wolmedica, jako jedyny ośrodek medyczny w województwie świętokrzyskim
+    wykonuje trójwymiarowe modele płodów. Jest to możliwe, dzięki specjalnej drukarce 3D,
+    która na podstawie danych z USG 3D , tworzy rzeźbę rozwijającego się dziecka. Model może
+    być wyjątkowa pamiątką z okresu ciąży.
+    By powstał, konieczne jest by ciężarna w 11-13 albo w 22-34 tygodniu ciąży przeszła badanie
+    USG 3D. Obraz zostanie zapisany jako plik odpowiedniego formatu, a następnie
+    przetworzony przez specjalne oprogramowanie. Na tej podstawie powstaje niezwykle
+    precyzyjny model płodu.
+      </p>
+      <img src="./assets/images/modal/model3d/model3d.jpeg" width="80%" alt="model3d">
+      <img src="./assets/images/modal/model3d/model3d2.jpg" width="80%" alt="model3d">
+      <img src="./assets/images/modal/model3d/model3d3.jpg" width="80%" alt="model3d">
+      <img src="./assets/images/modal/model3d/model3d4.jpg" width="80%" alt="model3d">
+      <img src="./assets/images/modal/model3d/model3d5.jpg" width="80%" alt="model3d">
+
+
+    `,
+  },
+
+  radiologiczne: {
+    title: "Badania radiologiczne",
+    description: `<p class='paragraph'>Badania ultrasonograficzne są nieinwazyjne i bezpieczne, gdyż wykorzystują fale
+    ultradźwiękowe.
+    USG pozwalają na szczegółowe i obrazowe pokazanie wielkości, kształtów poszczególnych
+    organów m.in.: piersi, tarczycy, brzucha, węzłów chłonnych. A u najmniejszych dzieci
+    również ocenę bioder, czy przezciemiączkowo: ośrodkowego układu nerwowego.
+    Wolmedica dysponuje wysokiej klasy aparatami USG.
+    
+    W trakcie tych badań, możliwa jest ocena stanu narządów i wykrycie wszelkich, również
+    niebezpiecznych zmian w naszym organizmie.
+    W Wolmedice dysponujemy również wielofunkcyjnym aparatem rentgenowskim,
+    pantomogramem, który pozwala uzyska c obraz zębów i żuchwy u pacjentów przed
+    założeniem aparatu ortodontycznego. Cefalogramem obrazuje z kolei czaszkę, tkanki twarde i
+    miękkie twarzy, podniebienie twarde oraz zatoki przynosowe. Ta odmiana zdjęć RTG jest
+    również wykorzystywana w ortodoncji.
+    
+    </p>
+    `,
+  },
+
+  pobraniekrwi: {
+    title: "Pobrania krwi",
+    description: `<p class='paragraph'>W Centrum Medycznym Wolmedica przy ulicy Zagnańskiej, sześć dni w tygodniu (od
+      poniedziałku do soboty) działa punkt pobrań krwi, gdzie można zrealizować szeroki pakiet
+      badań laboratoryjnych.
+      Posiadamy wykwalifikowany personel, wyspecjalizowany w pobraniach krwi u dzieci, w tym
+      noworodków i niemowląt. Można nam zaufać.
+      Pobrania odbywają się w przyjaznej atmosferze i w komfortowych dla pacjenta warunkach.
+      Wyniki badań są dostępne on – line.
+      
+    </p>
+    `,
+  },
+
+  prowadzenieciazy: {
+    title: "Prowadzenie ciąży",
+    description: `<p class='paragraph'>Opiekę nad ciężarną staramy rozpocząć się jak najwcześniej. W naszym centrum, przyszłe mamy czeka wybór ginekologa, który poprowadzi je przez cały okres ciąży.
+
+    Częstość wizyt jest indywidualnie dostosowana do potrzeb pacjentki oraz do stopnia ewentualnego ryzyka medycznego. Wizyty odbywają się w intymnej i przyjaznej atmosferze. Prowadzenie ciąży jest zindywidualizowane i dostosowane do potrzeb i oczekiwań kobiety.
+    </p>
+    `,
+  },
+
+  douzycia: {
+    title: "Badania radiologiczne",
+    description: `<p class='paragraph'>
+    </p>
+    `,
+  },
+
+  douzycia: {
+    title: "Badania radiologiczne",
+    description: `<p class='paragraph'>
+    </p>
+    `,
   },
 };
 
